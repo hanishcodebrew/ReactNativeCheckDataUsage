80f3f9b1a88d2b94f0172310cdf01f55ab232d89
Shubham Dhingra <shubhamd@code-brew.com>
2018-07-02 17:16:23 +0530

    initial commit

--- android/src/main/java/com/reactlibrary/RNCheckDataUsageModule.java
+++ android/src/main/java/com/reactlibrary/RNCheckDataUsageModule.java
@@ -17,13 +17,20 @@ public class RNCheckDataUsageModule extends ReactContextBaseJavaModule {
   }
 
   @ReactMethod
-  public void showToast(String message) {
-    Toast.makeText(reactContext, message, 
-    Toast.LENGTH_LONG).show();
+  public String checkDataUsage(String message) {
+   return ""+getFileSize(android.net.TrafficStats.getTotalRxBytes());
   }
-  
+
   @Override
   public String getName() {
     return "RNCheckDataUsage";
   }
+
+  public static String getFileSize(long size) {
+    if (size <= 0)
+        return "0";
+    final String[] units = new String[] { "B", "KB", "MB", "GB", "TB" };
+    int digitGroups = (int) (Math.log10(size) / Math.log10(1024));
+    return new DecimalFormat("#,##0.#").format(size / Math.pow(1024, digitGroups)) + " " + units[digitGroups];
+}
 }
